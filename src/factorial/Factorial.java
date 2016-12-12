f827fca2b5506d0f9ea9f0d3e30697979df6a6e5
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-12-12 09:20:14 +0100

    Formateamos el codigo

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,38 +1,26 @@
-
 package factorial;
-
 
 public class Factorial {
 
-
     public static void main(String[] args) {
 
+        int numero;
+        int factorial;
 
-    int numero;
-    int factorial;
+        numero = 8;
 
-  
-   numero = 8;    
-   
+        int contador;
+        if (numero == 0) {
+            factorial = 1;
+        } else {
+            factorial = 1;
+            for (contador = numero; contador >= 1; contador--) {
+                factorial = factorial * contador;
+            }
+        }
 
-    
-   int contador;
-   if (numero == 0)
-factorial = 1;
-   else
-   {
-factorial = 1;
-for (contador = numero; contador >= 1; contador--)
-{
-    factorial = factorial * contador;
-}
-   }    
-  
- 
-
-System.out.println(factorial); 
-
+        System.out.println(factorial);
 
     }
-    
+
 }
